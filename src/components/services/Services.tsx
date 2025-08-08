4c29ad0c1f18536c26174f6b39a9b8ee3efb9490
Rien van der Scheur <Rvdscheur@on-track.nl>
2025-08-08 20:01:39 +0200

    fixed-spacing

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,9 +1,6 @@
 
 import { Container } from '@mantine/core';
 import classes from './Services.module.css';
-
-
-
 import { Process } from './Process';
 import { Consultancy } from './Consultancy';
 import { Software } from './Software';
